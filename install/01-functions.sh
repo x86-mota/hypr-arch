a6fa06333e5b03f8331f22f5051d3388746f22ed
x86mota <felipecruz.d@proton.me>
2024-08-06 12:09:18 -0300

    Install Package from Arch repositories or AUR

--- install/01-functions.sh
+++ install/01-functions.sh
@@ -46,3 +46,31 @@ function _IsAURAvailable {
         return 1
     fi
 }
+
+# ----------------------------------------------------------------------- #
+#               Install Package from Arch repositories or AUR             #
+# ----------------------------------------------------------------------- #
+function _InstallPackage {
+    if ! _IsInstalled "$1"; then
+        if _IsPacmanAvailable "$1"; then
+            echo -e "[${BLUE}NOTE${RC}] - Installing $1..." 2>&1 | tee -a "${INSTALL_LOG}"
+            sudo pacman -S --noconfirm --needed "$1" &>>"${INSTALL_LOG}"
+
+        elif _IsAURAvailable "$1"; then
+            echo -e "[${BLUE}NOTE${RC}] - Installing $1 from AUR. This may take a while..." 2>&1 | tee -a "${INSTALL_LOG}"
+            ${AUR_HELPER} -S --noconfirm --needed "$1" &>>"${INSTALL_LOG}"
+        else
+            echo -e "${CL}[${RED}ERROR${RC}] - Unknown package $1." 2>&1 | tee -a "${INSTALL_LOG}"
+            return
+        fi
+
+        if _IsInstalled "$1"; then
+            echo -e "${CL}[${GREEN}OK${RC}] - $1 installed." 2>&1 | tee -a "${INSTALL_LOG}"
+        else
+            echo -e "${CL}[${RED}ERROR${RC}] - $1 install had failed, please check the install.log" 2>&1 | tee -a "${INSTALL_LOG}"
+            exit 1
+        fi
+    else
+        echo -e "[${BLUE}NOTE${RC}] - $1 already installed." 2>&1 | tee -a "${INSTALL_LOG}"
+    fi
+}

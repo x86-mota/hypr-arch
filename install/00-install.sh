315be9ada83e4bbdbdfc2289e74301a7d066471c
x86mota <felipecruz.d@proton.me>
2024-08-06 11:56:44 -0300

    add main install script

---
+++ install/00-install.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+clear
+echo -e "${ASCII_ART}"
+
+# --------------------------------------------------------------------- #
+#               Load functions used throughout the script               #
+# --------------------------------------------------------------------- #
+source ./01-functions.sh

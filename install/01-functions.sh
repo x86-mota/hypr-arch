906d03ccd02f7d0a20f982c201506294880926c1
x86mota <felipecruz.d@proton.me>
2024-08-06 12:00:14 -0300

    add function to check if the parameter was added or edited successfully into fila

--- install/01-functions.sh
+++ install/01-functions.sh
@@ -1,2 +1,15 @@
 #!/usr/bin/env bash
 
+# ------------------------------------------------------------------------------------------ #
+#               Checks if the parameter was added/edited successfully into file              #
+# ------------------------------------------------------------------------------------------ #
+function _IsAdded {
+    VALUE="${1/^/}"
+    if grep -qw -- "$1" "$2"; then
+        echo -e "[${GREEN}OK${RC}] - '${VALUE}' successfully added to $2" 2>&1 | tee -a "${INSTALL_LOG}"
+        return 0
+    else
+        echo -e "[${RED}ERROR${RC}] - '${VALUE}' was not added to the file $2" 2>&1 | tee -a "${INSTALL_LOG}"
+        return 1
+    fi
+}

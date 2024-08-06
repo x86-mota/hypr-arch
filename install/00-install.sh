888a00be0f7fb8324b068d5d55c99f55fdae9e32
x86mota <felipecruz.d@proton.me>
2024-08-06 12:47:28 -0300

    add argument

--- install/00-install.sh
+++ install/00-install.sh
@@ -6,13 +6,13 @@ echo -e "${ASCII_ART}"
 # --------------------------------------------------------------------- #
 #               Load functions used throughout the script               #
 # --------------------------------------------------------------------- #
-if _CheckFileExist; then
+if _CheckFileExist "./01-functions.sh"; then
     source ./01-functions.sh
 fi
 
 # ------------------------------------------------------------- #
 #               Load package lists for installation             #
 # ------------------------------------------------------------- #
-if _CheckFileExist; then
+if _CheckFileExist "./02-packages.sh"; then 
     source ./02-packages.sh
 fi

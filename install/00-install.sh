f9568261979f4a72a494f08a92ccd1ad4efb190c
x86mota <felipecruz.d@proton.me>
2024-08-06 12:56:25 -0300

    add funtion to clear lines

--- install/00-install.sh
+++ install/00-install.sh
@@ -16,3 +16,32 @@ fi
 if _CheckFileExist "./02-packages.sh"; then 
     source ./02-packages.sh
 fi
+
+# ----------------------------------------------------------------------------------------------------- #
+#               Let the user choose which AUR helper to use if none is already installed                #
+# ----------------------------------------------------------------------------------------------------- #
+AUR_HELPER_LIST=("paru" "yay")
+for HELPR in "${AUR_HELPER_LIST[@]}"; do
+    if _IsInstalled "$HELPR" &>/dev/null; then
+        AUR_HELPER="$HELPR"
+        echo -e "[${BLUE}NOTE${RC}] - AUR helper ${AUR_HELPER} found" 2>&1 | tee -a "${INSTALL_LOG}"
+        break
+    fi
+done
+
+if [ -z "$AUR_HELPER" ]; then
+    echo -e "${YELLOW}ACTION${RC} - Which AUR helper? (default = 1):\n 1) yay\n 2) paru"
+    read -rp "Enter your choice [1-2]: "
+    case "$REPLY" in
+    1)
+        AUR_HELPER="yay"
+        ;;
+    2)
+        AUR_HELPER="paru"
+        ;;
+    *)
+        AUR_HELPER="yay"
+        ;;
+    esac
+    echo -e "${CL}${CL}${CL}${CL}[${BLUE}NOTE${RC}] - ${AUR_HELPER} Selected" 2>&1 | tee -a "${INSTALL_LOG}"
+fi

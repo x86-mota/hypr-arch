ab48a4c233d6bd0763eca1e4d69706d59b1c9114
x86-mota <felipecruz.d@proton.me>
2024-08-19 19:37:25 -0300

    remove ascii_art variable

--- setup.sh
+++ setup.sh
@@ -11,25 +11,6 @@ declare -r BoldYellow='\033[1;33m'
 declare -r BoldBlue='\033[1;34m'
 declare -r Reset='\033[0m'
 declare -r Clear='\033[1A\033[K'
-ASCII_ART="${BoldGreen}
-
-                    -@                           
-                   .##@                          
-                  .####@                         
-                  @#####@                        
-                . *######@                       
-               .##@o@#####@                                                                ,,                           ,,  
-              /############@                      \`7MMF'  \`7MMF'                         \`7MM                         \`7MM  
-             /##############@             M         MM      MM                             MM                           MM  
-            @######@**%######@            M         MM      MM \`7M'   \`MF\`7MMpdMAo\`7Mb,od8 MM  ,6\"Yb. \`7MMpMMMb.   ,M\"\"bMM  
-           @######\`     %#####o       mmmmMmmmm     MMmmmmmmMM   VA   ,V   MM   \`Wb MM' \"' MM 8)   MM   MM    MM ,AP    MM  
-          @######@       ######%          M         MM      MM    VA ,V    MM    M8 MM     MM  ,pm9MM   MM    MM 8MI    MM  
-        -@#######h       ######@.\`        M         MM      MM     VVV     MM   ,AP MM     MM 8M   MM   MM    MM \`Mb    MM  
-       /#####h**\`\`       \`**%@####@               .JMML.  .JMML.   ,V      MMbmmd'.JMML. .JMML\`Moo9^Yo.JMML  JMML.\`Wbmd\"MML.
-      @H@*\`                    \`*%#@                              ,V       MM                                               
-     *\`                            \`*                          OOb\"      .JMML.                                             
-
-${Reset}"
 
 # ------------------------------------------------- #
 #               Checks if file exists               #
@@ -48,7 +29,24 @@ function _CheckFileExist {
 # --------------------------------- #
 
 clear
-echo -e "${ASCII_ART}"
+echo -e "${BoldGreen}
+                    -@                           
+                   .##@                          
+                  .####@                         
+                  @#####@                        
+                . *######@                       
+               .##@o@#####@                                                                ,,                           ,,  
+              /############@                      \`7MMF'  \`7MMF'                         \`7MM                         \`7MM  
+             /##############@             M         MM      MM                             MM                           MM  
+            @######@**%######@            M         MM      MM \`7M'   \`MF\`7MMpdMAo\`7Mb,od8 MM  ,6\"Yb. \`7MMpMMMb.   ,M\"\"bMM  
+           @######\`     %#####o       mmmmMmmmm     MMmmmmmmMM   VA   ,V   MM   \`Wb MM' \"' MM 8)   MM   MM    MM ,AP    MM  
+          @######@       ######%          M         MM      MM    VA ,V    MM    M8 MM     MM  ,pm9MM   MM    MM 8MI    MM  
+        -@#######h       ######@.\`        M         MM      MM     VVV     MM   ,AP MM     MM 8M   MM   MM    MM \`Mb    MM  
+       /#####h**\`\`       \`**%@####@               .JMML.  .JMML.   ,V      MMbmmd'.JMML. .JMML\`Moo9^Yo.JMML  JMML.\`Wbmd\"MML.
+      @H@*\`                    \`*%#@                              ,V       MM                                               
+     *\`                            \`*                          OOb\"      .JMML.                                             
+
+${Reset}"
 
 # --------------------------------------------------------- #
 #               Verify the system is Arch Linux             #
